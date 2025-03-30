d14431c86ab02db5a028ba6102f8702169dd7c41
Yashrajp8 <s4034050@student.rmit.edu.au>
2025-03-30 23:13:40 +1100

    do it

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import './ProjectDetail.css';
-import { fetchProjectById } from '../utils/supabase';
+import { fetchProjectById, fetchProjects } from '../utils/supabase';
 import ScrambleButton from './ScrambleButton';
 import Footer from './Footer';
 
 function ProjectDetail() {
   const { projectId } = useParams();
   const [project, setProject] = useState(null);
+  const [projects, setProjects] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [scrollPosition, setScrollPosition] = useState(0);
