3cc0f8f7cb4a830557cc5c4e34d51aa405befc97
Manan <manan@arintra.com>
2025-04-12 17:48:28 +0530

    Updated zIndex

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -1194,7 +1194,7 @@ function SplashCursor({
         position: 'fixed',
         top: 0,
         left: 0,
-        zIndex: 50,
+        zIndex: -1,
         pointerEvents: 'none',
         width: '100%',
         height: '100%',
